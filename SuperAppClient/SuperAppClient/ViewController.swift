839e991b82bcebc85892f29abb9e7d039072586f
Артем Валиев <a_valiev@controlsystems.ru>
2016-04-07 14:29:24 +0300

    deleted chat

--- SuperAppClient/SuperAppClient/ViewController.swift
+++ SuperAppClient/SuperAppClient/ViewController.swift
@@ -7,11 +7,31 @@
 //
 
 import UIKit
+import SocketIOClientSwift
+
+
+let socket = SocketIOClient(socketURL: NSURL(string: "http://localhost:3000")!, options: [.Log(true), .ForcePolling(true)])
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        socket.on("connect") {data, ack in
+            print("socket connected")
+        }
+        
+        socket.on("joinResult") {data, ack in
+            print("joinResult")
+        }
+        
+
+        
+        socket.connect()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
